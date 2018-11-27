15aa75dd7d7fa37228f5a3c32ecb166057e1095c
chris724uwm <chris724@uwm.edu>
2018-11-27 14:33:13 -0600

    added more tests for delete and create account

--- TAProject/djangoAccountTester.py
+++ TAProject/djangoAccountTester.py
@@ -22,6 +22,11 @@ class testDjangoAccount(TestCase):
     def test_createAccount_wrong_permissions(self):
         self.assertEquals(self.TAAccount.create_account(['user', 'pass', 'name', 'address', 'email', '2134235', 0]), "Account Creation Error: You don't have permission")
 
+    def test_createAccount_already_exists(self):
+        self.superAccount.create_account(['user', 'pass', 'name', 'address', 'email', '1234567890', 0])
+        self.assertEquals(self.superAccount.create_account(['user', 'pass', 'name', 'address', 'email', '1234567890', 0]),
+                          "Account Creation Error: Username Already Exists")
+
     def test_deleteAccount_deletes(self):
         Account.create_account(self.superAccount, ['user', 'pass', 'name', 'address', 'email', '1234567890', 0])
         Account.delete_account(self.superAccount, ['user'])
@@ -29,3 +34,12 @@ class testDjangoAccount(TestCase):
 
     def test_deleteAccount_wrong_permissions(self):
         self.assertEquals(self.TAAccount.delete_account(['user']), "Account Deletion Error: You don't have permission")
+
+    def test_deleteAccount_does_not_exist(self):
+        self.assertEquals(self.superAccount.delete_account(['userdoesnotexist']), "Account Deletion Error: No Such Account Exists")
+
+    def test_deleteAccount_not_enough_perms(self):
+        self.assertEquals(self.superAccount.delete_account([]),"Account Deletion Error: Wrong amount of information passed")
+
+    def test_deleteAccount_username_not_str(self):
+        self.assertEquals(self.superAccount.delete_account([1]), "Account Deletion Error: Incorrect username")
